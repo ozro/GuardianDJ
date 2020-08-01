a47068327c50c70bbd217764f5509965f0b625b0
Oliver Zhang <oliverzhang1@yahoo.com>
2020-08-01 13:01:56 -0700

    Initial commit with working platformer breadboard.

---
+++ Assets/Scripts/MCController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCController : MonoBehaviour
+{
+    CharacterController controller;
+    Animator animator;
+    SpriteRenderer sprite;
+
+    [SerializeField]
+    Canvas gameoverMenu = null;
+
+    [SerializeField]
+    float maxSpeed = 0.25f;
+    [SerializeField]
+    float acceleration = 0.25f;
+    [SerializeField]
+    float deceleration = 1f;
+    [SerializeField]
+    float jumpSpeed = 0.5f;
+    [SerializeField]
+    float gravity = 1f;
+
+    [SerializeField]
+    Vector3 desiredVel = Vector3.zero;
+
+    [SerializeField]
+    bool dead = false;
+    [SerializeField]
+    float deathTime = 2f;
+    float currentTime;
+
+    public void Kill()
+    {
+        dead = true;
+        currentTime = 0;
+    }
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>(); 
+        animator = GetComponent<Animator>(); 
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        gameoverMenu.gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        if (!dead)
+        {
+            UpdateInputs();
+            if(transform.position.y < -5)
+            {
+                Kill();
+            }
+        }
+        else
+        {
+            currentTime += Time.deltaTime;
+            if(currentTime > deathTime)
+            {
+                gameoverMenu.gameObject.SetActive(true);
+            }
+            desiredVel.y = -0.05f;
+            if (controller.isGrounded)
+            {
+                desiredVel.x *= 0.95f;
+            }
+        }
+        if (!controller.isGrounded){
+            desiredVel.y -= gravity * Time.deltaTime;
+        }
+        animator.SetBool("IsDead", dead);
+        animator.SetBool("IsGrounded", controller.isGrounded);
+        animator.SetFloat("SpeedH", Mathf.Abs(desiredVel.x));
+        controller.Move(desiredVel);
+        animator.SetFloat("VelocityV", controller.velocity.y);
+    }
+    void UpdateInputs()
+    {
+        //float h = Input.GetAxis("Horizontal");
+        float h = 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            h = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            h = 1;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Kill();
+        }
+
+        if(h==0) 
+        {
+            if(desiredVel.x > deceleration * Time.deltaTime)
+            {
+                desiredVel.x -= deceleration * Time.deltaTime;
+            }
+            else if(desiredVel.x < -deceleration * Time.deltaTime)
+            {
+                desiredVel.x += deceleration * Time.deltaTime;
+            }
+            else
+            {
+                desiredVel.x = 0;
+            }
+        }
+        else if((h<0 && desiredVel.x > 0) || (h>0 && desiredVel.x < 0))
+        {
+            if(h < 0)
+            {
+                desiredVel.x -= deceleration * Time.deltaTime;
+            }
+            else if(h>0)
+            {
+                desiredVel.x += deceleration * Time.deltaTime;
+            }
+        }
+        else if(h>0 && desiredVel.x < maxSpeed)
+        {
+            desiredVel.x += acceleration * Time.deltaTime;
+        }
+        else if(h<0 && desiredVel.x > -maxSpeed)
+        {
+            desiredVel.x -= acceleration * Time.deltaTime;
+        }
+        desiredVel.x = Mathf.Clamp(desiredVel.x, -maxSpeed, maxSpeed);
+        if (h != 0)
+        {
+            transform.right = Vector3.right * Mathf.Sign(h);
+        }
+        if (controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                desiredVel.y = jumpSpeed;
+                animator.SetTrigger("Jumped");
+            }
+            else
+            {
+                desiredVel.y = -0.05f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracker : MonoBehaviour
+{
+    [SerializeField]
+    CharacterController target = null;
+    [SerializeField]
+    float trackDelay;
+
+    Vector3 trackVel;
+
+    float separation = 0;
+
+    private void Start()
+    {
+        separation = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 pos = target.transform.position;
+        pos.z = separation;
+        transform.position = pos;
+    }
+}
