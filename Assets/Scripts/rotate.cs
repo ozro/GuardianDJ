75ec4e7d057da6f766c828ede2e7b7bc864b2094
sap1l <30491887+sap1l@users.noreply.github.com>
2020-08-03 15:37:29 +1000

    added record player

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{	
+	// audio
+	private AudioSource source;
+	private float audioLength = 359.549f;
+	
+	// auto rotation
+	private float rotZ;
+	public float rotationSpeed;
+	private bool mouseUp = true;
+	
+	// mouse drag rotation
+	private float baseAngle = 0.0f;
+	
+	public float currentAngle = 0.0f;
+	
+	void Start() {
+		source = GetComponent<AudioSource>();
+		source.Play();
+		
+		rotationSpeed = audioLength / 360;
+	}
+
+	void Update() {
+		// continue to rotate as long as mouse isn't clicked on object
+		if (mouseUp) {
+			rotZ += -Time.deltaTime * rotationSpeed;
+			transform.rotation = Quaternion.Euler(0, 0, rotZ);
+		}
+		// if we rotate past 360 we reset back towards 0 to keep angles within 360
+		if (rotZ <= -360) {
+			rotZ += 360;
+		}
+		// grab the new rotation (if mouse rotated it)
+		rotZ = -360 + transform.eulerAngles.z;
+		
+		currentAngle = 360 - transform.eulerAngles.z;
+	}
+
+    void OnMouseDown() {
+		mouseUp = false;
+		Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+		pos = Input.mousePosition - pos;
+		baseAngle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+		baseAngle -= Mathf.Atan2(transform.right.y, transform.right.x) *Mathf.Rad2Deg;
+		
+		source.Pause();
+    }
+
+    void OnMouseDrag() {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        pos = Input.mousePosition - pos;
+        float angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - baseAngle;
+		transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+	
+	void OnMouseUp() {
+		mouseUp = true;
+		
+		// calc new audio position
+		source.time = ((360 - transform.eulerAngles.z) / 360) * audioLength;
+		
+		source.Play();
+	}
+}
