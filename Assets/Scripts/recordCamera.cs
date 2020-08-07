3eb2ffdcfd96efdc5d3d72ad2016f4a0bf3716c6
sap1l <30491887+sap1l@users.noreply.github.com>
2020-08-07 14:41:43 +1000

    merged record + main

---
+++ Assets/Scripts/recordCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recordCamera : MonoBehaviour
+{
+	public Camera recordCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+		Rect camRect = recordCam.rect;
+		camRect.yMax = 0.4f; // 60% of viewport
+		recordCam.rect = camRect;
+    }
+}
