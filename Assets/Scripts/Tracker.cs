f790672156a44fe6b7825c3b49ca0490068d4f3c
Oliver Zhang <oliverzhang1@yahoo.com>
2020-08-07 19:49:03 -0700

    Added UI and camera split

--- Assets/Scripts/Tracker.cs
+++ Assets/Scripts/Tracker.cs
@@ -26,9 +26,6 @@ public class Tracker : MonoBehaviour
     private void Start()
     {
 		separation = transform.position.z;
-		Rect camRect = Camera.main.rect;
-		camRect.yMin = 0.4f; // 60% of viewport
-		Camera.main.rect = camRect;
     }
 
     private void OnDrawGizmos()
